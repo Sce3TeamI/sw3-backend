ce52381a9c074fd45b1afacae236d96fcd813709
chilichilimo <afshar.m.95@gmail.com>
2016-11-03 15:47:54 +0000

    fix the error

--- app/app.js
+++ app/app.js
@@ -94,7 +94,7 @@ app.use('/api', router);
 app.listen(API_PORT);
 
 router.get('/',function(req, res){
-    console.log('I am Here yay');
+    console.log('SHIT');
 });
 
 router.get('/loginUser', function(req, res) {
